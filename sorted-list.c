59feaff098ef08b06653e8b6c0a298f7f5004b0f
Cal <cal13199@gmail.com>
2015-10-03 19:17:57 -0400

    Style

--- sorted-list.c
+++ sorted-list.c
@@ -6,20 +6,20 @@
 
 SortedListPtr SLCreate(CompareFuncT cf, DestructFuncT df)
 {
-	SortedListPtr list=				malloc(sizeof(SortedListPtr));
-	Node *listNode=					malloc(sizeof(Node));
+	SortedListPtr list=					malloc(sizeof(SortedListPtr));
+	Node *listNode=						malloc(sizeof(Node));
         
 	list->comp=						cf;
 	list->dest=						df;
 	list->node=						listNode;
-	list->node->next=				NULL;
+	list->node->next=					NULL;
 		
 	return list;
 }
 
 void SLDestroy(SortedListPtr list)
 {
-	struct Node *current=			list->node;
+	struct Node *current=					list->node;
 	struct Node *next;
     
 	if (current==NULL)
@@ -32,7 +32,7 @@ void SLDestroy(SortedListPtr list)
 		free(current);
 		free(list);
 		return;
-    }
+	}
 	else
 	{
 		while (current!=NULL)
@@ -40,7 +40,7 @@ void SLDestroy(SortedListPtr list)
 			next = 					current->next;
 			free(current);
 			current=				next;
-	   }
+		}
 	   
 	free(list);
 	return;
@@ -48,44 +48,42 @@ void SLDestroy(SortedListPtr list)
 }
 int SLInsert(SortedListPtr list, void *newObj)
 {
-    /*  Init the insertion node
-        Also inits a traversal node
-    */
-    
-    /* 
-        Node to be inserted and iterator node respecitvely 
-    */
+	/*  
+		Init the insertion node
+		Also inits a traversal node
+		Node to be inserted and iterator node respectively 
+	*/
 	
-	struct Node *toInsert= 			malloc(sizeof(Node));
-	struct Node *iter=				list->node; 
+	struct Node *toInsert= 					malloc(sizeof(Node));
+	struct Node *iter=					list->node; 
 
-	toInsert->data=					newObj;
+	toInsert->data=						newObj;
     
-    /* 
+	/* 
 		If the list is empty
-    */
+	*/
 	if (iter==NULL)
 	{ 
 		
-		toInsert->previous=			NULL;
-		toInsert->next=				NULL;
+		toInsert->previous=				NULL;
+		toInsert->next=					NULL;
 		list->node=					toInsert;
 		return 1;
 	}
 	/*
-		If the object is the largest
+		If the object is the largest immediatley
 	*/
 	else if (list->comp(newObj,iter->data)==1)
 	{
-		toInsert->previous=			NULL;
-		toInsert->next=				iter;
-		iter->previous=				toInsert;
+		toInsert->previous=				NULL;
+		toInsert->next=					iter;
+		iter->previous=					toInsert;
 		list->node=					toInsert;
 		return 1;		
 	}
-    /* 
+	/* 
 		If the object is already in the list, and it's the first thing there
-    */
+	*/
 	else if (list->comp(newObj,iter->data)==0)
 	{
 		return 0;
@@ -94,52 +92,52 @@ int SLInsert(SortedListPtr list, void *newObj)
 	{
 		while (iter!=NULL)
 		{
-
-			/* 
+	/* 
                 if it exists already, don't insert. Return failure to insert
-			*/
-            
+	*/
 			if (list->comp(iter->data,newObj)==0)
 			{
 				return 0;
 			}
-			/*
-				If it't smaller than iter, insert it there
-			*/
+	/*
+		If the next object is smaller. Insert there
+	*/
 			else if (list->comp(newObj,iter->next->data)==1)
-            {
+			{
 	
-				toInsert->next=				iter->next;
-				toInsert->previous=			iter;
+				toInsert->next=			iter->next;
+				toInsert->previous=		iter;
 				toInsert->next->previous=	toInsert;
 				iter->next=toInsert;
-                return 1;	
+				return 1;	
 			}
-            else if (iter->next==NULL)
-            {
-				iter->next=					toInsert;
-				toInsert->previous=			iter;
-				toInsert->next=				NULL;
+			else if (iter->next==NULL)
+			{	
+				iter->next=			toInsert;
+				toInsert->previous=		iter;
+				toInsert->next=			NULL;
 				
 				return 1;
 
-            }
-			
-			iter=					iter->next;
 		}
+			
+		iter=						iter->next;
+	}
         return 1;
 	}
 }
 int SLRemove(SortedListPtr list, void *newObj)
 {
-	struct Node *current=			list->node;
+	struct Node *current=					list->node;
 	
 	if (list->comp(current->data,newObj)==0)
 	{
-	/*Assuming the head is the object to remove */
+	/*
+		Assuming the head is the object to remove 
+	*/
 		if (current->next!=NULL)
 		{
-			current->next->previous=NULL;
+			current->next->previous=		NULL;
 			list->node=				current->next;
 			free(current);
 			return 1;
@@ -159,22 +157,19 @@ int SLRemove(SortedListPtr list, void *newObj)
 
 			if (list->comp(current->data,newObj)==0)
 			{
-				current->previous->next=current->next;
+				current->previous->next=	current->next;
 				free(current);
 				return 1;
+			}
 		}
+		return 0;
 	}
-        return 0;
-	}
-    /*
-    return 0;
-    */
 }
 
 
 SortedListIteratorPtr SLCreateIterator(SortedList *list)
 {
-	SortedListIteratorPtr iterator= 	malloc(sizeof(SortedListIteratorPtr));
+	SortedListIteratorPtr iterator= 			malloc(sizeof(SortedListIteratorPtr));
 	iterator->iterNode=					list->node;
 	return iterator;
 }
@@ -187,7 +182,7 @@ void SLDestroyIterator(SortedListIteratorPtr iter)
 
 void * SLNextItem(SortedListIteratorPtr iter)
 {
-	iter->iterNode=iter->iterNode->next;
+	iter->iterNode=						iter->iterNode->next;
     
     if (iter->iterNode==NULL)
         return NULL;
