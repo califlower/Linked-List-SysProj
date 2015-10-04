da280e6b2a4e34aa0e5c6d12a928e542fa99e290
Cal <cal13199@gmail.com>
2015-10-04 17:34:13 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,24 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int compareIntegerTest(void *p1, void *p2)
-{
-	int firstInt 			= *(int *)p1;
-	int secondInt 			= *(int *)p2;
-	
-	if(firstInt < secondInt)
-	{
-		return -1;
-	}
-	else if(firstInt > secondInt)
-	{
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
-}
 int compareDoubleTest(void *p1, void *p2)
 {
 	double firstDouble		= *(double *)p1;
@@ -45,7 +27,7 @@ void destruct(void *a)
 int main(int argc, char **argv) 
 {
 	/*
-		Test cases for double numbers
+		Test cases for ints numbers
 	*/
 	
 	SortedListPtr sortedList = SLCreate(&compareDoubleTest, &destruct);
@@ -53,35 +35,150 @@ int main(int argc, char **argv)
 	double *n2 = malloc(sizeof(double*));
 	double *n3 = malloc(sizeof(double*));
 	double *n4 = malloc(sizeof(double*));
-	double *n5 = malloc(sizeof(double*));
-	double *n6 = malloc(sizeof(double*));
 	
-	*n1 = 5.0;
-	*n2 = 10.0;
-	*n3 = 3.0;
-	*n4 = 8.0;
-	*n5 = 11.0;
-	*n6 = 1.0;
+	
+	*n1 = 1;
+	*n2 = 1;
+	*n3 = 1;
+	*n4 = 1;
+	
 	
 	SLInsert(sortedList, n1);
 	SLInsert(sortedList, n2);
-	SLInsert(sortedList, n5);
 	SLInsert(sortedList, n3);
-	SLInsert(sortedList, n6);
+	SLInsert(sortedList, n4);
 	displayDouble(sortedList);
+	SLRemove(sortedList,n1);
 	
-	/*
+	/*2*/
+	*n1 = 1;
+	*n2 = 2;
+	*n3 = 3;
+	*n4 = 4;
+	
+	
+	SLInsert(sortedList, n1);
+	SLInsert(sortedList, n2);
+	SLInsert(sortedList, n3);
+	SLInsert(sortedList, n4);
+	displayDouble(sortedList);
 	SLRemove(sortedList, n1);
 	SLRemove(sortedList, n2);
-	SLRemove(sortedList, n5);
 	SLRemove(sortedList, n3);
-	SLRemove(sortedList, n6);
+	SLRemove(sortedList, n4);
 	
 	
-	display(sortedList);
-	
-	*/
 	
 	
-	return 0;
+	/*3*/
+	*n1 = 4;
+	*n2 = 2;
+	*n3 = 4;
+	*n4 = 2;
+	
+	
+	SLInsert(sortedList, n1);
+	SLInsert(sortedList, n2);
+	SLInsert(sortedList, n3);
+	SLInsert(sortedList, n4);
+	displayDouble(sortedList);
+	SLRemove(sortedList, n1);
+	SLRemove(sortedList, n2);
+	SLRemove(sortedList, n3);
+	SLRemove(sortedList, n4);
+	
+	
+	
+	/*4*/
+	*n1 = 9;
+	*n2 = 2;
+	*n3 = 10;
+	*n4 = 5;
+	
+	
+	SLInsert(sortedList, n1);
+	SLInsert(sortedList, n2);
+	SLInsert(sortedList, n3);
+	SLInsert(sortedList, n4);
+	displayDouble(sortedList);
+	SLRemove(sortedList, n1);
+	SLRemove(sortedList, n2);
+	SLRemove(sortedList, n3);
+	SLRemove(sortedList, n4);
+	
+	
+	/* Doubles Test */
+	printf("\n");
+	printf("Doubles Test Case");
+	printf("\n");
+	/*1*/
+	*n1 = 1.4;
+	*n2 = 1.4;
+	*n3 = 1.4;
+	*n4 = 1.4;
+	
+	
+	SLInsert(sortedList, n1);
+	SLInsert(sortedList, n2);
+	SLInsert(sortedList, n3);
+	SLInsert(sortedList, n4);
+	displayDouble(sortedList);
+	SLRemove(sortedList, n1);
+	SLRemove(sortedList, n2);
+	SLRemove(sortedList, n3);
+	SLRemove(sortedList, n4);
+	
+	/*2*/
+	*n1 = 1.1;
+	*n2 = 2.2;
+	*n3 = 3.3;
+	*n4 = 4.4;
+	
+	
+	SLInsert(sortedList, n1);
+	SLInsert(sortedList, n2);
+	SLInsert(sortedList, n3);
+	SLInsert(sortedList, n4);
+	displayDouble(sortedList);
+	SLRemove(sortedList, n1);
+	SLRemove(sortedList, n2);
+	SLRemove(sortedList, n3);
+	SLRemove(sortedList, n4);
+	
+	/*3*/
+	*n1 = 4.4;
+	*n2 = 2.2;
+	*n3 = 4.4;
+	*n4 = 2.2;
+	
+	
+	SLInsert(sortedList, n1);
+	SLInsert(sortedList, n2);
+	SLInsert(sortedList, n3);
+	SLInsert(sortedList, n4);
+	displayDouble(sortedList);
+	SLRemove(sortedList, n1);
+	SLRemove(sortedList, n2);
+	SLRemove(sortedList, n3);
+	SLRemove(sortedList, n4);
+	
+	
+	/*4*/
+	*n1 = 9.9;
+	*n2 = 2.2;
+	*n3 = 10.1;
+	*n4 = 5.5;
+	
+	
+	SLInsert(sortedList, n1);
+	SLInsert(sortedList, n2);
+	SLInsert(sortedList, n3);
+	SLInsert(sortedList, n4);
+	displayDouble(sortedList);
+	SLRemove(sortedList, n1);
+	SLRemove(sortedList, n2);
+	SLRemove(sortedList, n3);
+	SLRemove(sortedList, n4);
+	
+	return 1;	
 }
